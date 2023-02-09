f7e69322285dbe2b34a0894a53470df4d3aba400
Rahul Jain <rahul.jain.22.1@mountblue.tech>
2023-02-09 20:19:12 +0530

    Created User and Profile models

---
+++ app/Models/Profile.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Profile extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @column()
+  public firstName: string
+
+  @column()
+  public lastName: string
+
+  @column()
+  public mobileNumber: string
+
+  @column()
+  public gender: string
+
+  @column.date({
+    serialize: (value) => value.toFormat('DD-MM-YYYY'),
+  })
+  public dob: DateTime
+}
